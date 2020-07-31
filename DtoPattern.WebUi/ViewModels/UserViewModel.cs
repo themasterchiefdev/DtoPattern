44d0a9d01fd07e32c303080e824b4f2fe29d6c76
yvrkarthik <yvrkarthik@gmail.com>
2020-08-01 09:34:51 +1200

    Add User Domain Model and ViewModel

---
+++ DtoPattern.WebUi/ViewModels/UserViewModel.cs
@@ -0,0 +1,15 @@
+namespace DtoPattern.WebUi.ViewModels
+{
+    public class UserViewModel
+    {
+
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+
+    }
+}
